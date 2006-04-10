abce84f459457aff81017bae5ba7f12d601269d4
Justin Mason <jmason@cpan.org>
2006-04-10 20:30:27 +0000

    Import of JMASON/IPC-DirQueue-0.07 from CPAN.
    gitpan-cpan-distribution: IPC-DirQueue
    gitpan-cpan-version:      0.07
    gitpan-cpan-path:         JMASON/IPC-DirQueue-0.07.tar.gz
    gitpan-cpan-author:       JMASON
    gitpan-cpan-maturity:     released

--- t/Util.pm
+++ t/Util.pm
@@ -1,12 +1,30 @@
 
+our $perl_path;
+sub find_perl_path {
+
+  use Config;
+  if ($^X =~ m|^/|) {
+    $perl_path = $^X;
+  }
+  elsif ($config{PERL_PATH}) {
+    $perl_path = $config{PERL_PATH};
+  }
+  else {
+    $perl_path = $Config{perlpath};
+    $perl_path =~ s|/[^/]*$|/$^X|;
+  }
+}
+
 
 sub start_indexd {
   my $indexd_port = int (20000 + rand(10000));
   $indexd_uri = "dq://127.0.0.1:${indexd_port}";
 
+  find_perl_path();
+
   if (($indexd_pid = fork) == 0) {
-    print "running: ../dq-indexd --port $indexd_port\n";
-    exec ("../dq-indexd --port $indexd_port");
+    print "running: $perl_path ../dq-indexd --port $indexd_port\n";
+    exec ("$perl_path ../dq-indexd --port $indexd_port");
     die;
   } else {
     sleep 1;
@@ -17,11 +35,14 @@ sub start_indexd {
 sub stop_indexd {
   # use POSIX ":sys_wait_h";
 
+  # need to set this explicitly, otherwise t/15_enq_indexd.t screws up
+  $SIG{CHLD} = 'DEFAULT';
+
   ok kill (15, $indexd_pid);
   print "stopped indexd $indexd_pid at $indexd_uri\n";
 
   $kid = waitpid($indexd_pid, 0);
-  ok ($kid == $indexd_pid);
+  ok ($kid == $indexd_pid) or warn "kid=$kid ipid=$indexd_pid ex=$! q=$?";
 
   ok (($? >> 8) == 0);
 }
